863cf47de3832b38444bdad59727355f159ba939
Daniel Bryan <danbryan@gmail.com>
2013-12-05 15:10:43 +1100

    [UPDATE] Add README link to doc.go for godoc

--- doc.go
+++ doc.go
@@ -6,4 +6,6 @@
 // If the agentx master socket is not at /var/agentx/master,
 // agentx.MasterSocket must be changed prior to calling Run() for the first
 // time.
+//
+// See https://github.com/Learnosity/agentx/blob/master/README.md for more.
 package agentx
